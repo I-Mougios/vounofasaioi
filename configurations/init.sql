d117358e250cd6512aaf35e25c076f2edfaa1a0f
I-Mougios <i.mougios.tech@gmail.com>
2025-07-21 20:01:58 +0300

    chore(configs): update test configs

--- configurations/init.sql
+++ configurations/init.sql
@@ -1,3 +1,11 @@
+-- 1. Create the database
+CREATE DATABASE IF NOT EXISTS vounofasaioi;
+
+-- 2. Create the user (only if it doesn’t exist)
 CREATE USER IF NOT EXISTS 'imougios'@'%' IDENTIFIED WITH mysql_native_password BY 'user_password';
+
+-- 3. Grant access to the user on that database
 GRANT ALL PRIVILEGES ON vounofasaioi.* TO 'imougios'@'%';
+
+-- 4. Apply the privileges
 FLUSH PRIVILEGES;
