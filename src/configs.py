fe485ab7cb3c3ee9c0702a80912edc76588bbc6a
I-Mougios <i.mougios.tech@gmail.com>
2025-08-13 17:41:32 +0300

    feat(configs): add a custom bool_ function

--- src/configs.py
+++ src/configs.py
@@ -2,14 +2,17 @@ from icecream import ic
 
 from pyutils import ConfigMeta
 
+__all__ = ["DBConfig", "bool_"]
 
 class DBConfig(
     metaclass=ConfigMeta, config_filename="database.test.ini", config_directory="configurations"
 ):
     """Database configurations"""
 
+def bool_(value):
+    return True if value.casefold() == "true" else False
 
-if DBConfig.globals.get("icecream_enabled", False):
+if DBConfig.globals.get("icecream_enabled", cast=bool_):
     ic.enable()
 else:
     ic.disable()
