45d21d9a541037a1ef1f90e625e1cfe376d81c35
I-Mougios <i.mougios.tech@gmail.com>
2025-08-23 01:37:24 +0300

    chore(dependencies): fix type annotation

--- src/reservations/dependencies.py
+++ src/reservations/dependencies.py
@@ -1,4 +1,5 @@
 # src/reservations/dependencies.py
+from typing import Generator
 from sqlalchemy.orm import Session, sessionmaker
 
 from database.engine import engine
@@ -6,7 +7,7 @@ from database.engine import engine
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False, expire_on_commit=True)
 
 
-def open_session() -> Session:
+def open_session() -> Generator[Session,None,None]:
     session = SessionLocal()
     try:
         yield session
