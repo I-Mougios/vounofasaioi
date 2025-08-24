3e8201c8b300123537380b0940848ee47492f289
I-Mougios <i.mougios.tech@gmail.com>
2025-08-25 01:05:51 +0300

    refactor(database.engine): keep only the connection string

--- src/database/engine.py
+++ src/database/engine.py
@@ -1,6 +1,4 @@
 # src/database/engine.py
-from pathlib import Path
-
 import sqlalchemy as sa
 from icecream import ic
 
@@ -8,21 +6,12 @@ from configs import DBConfig, bool_
 
 __all__ = ["engine"]
 
-base_dir = Path(__file__).resolve().parents[0]
-
 username = DBConfig.user.get("username")
 password = DBConfig.user.get("password")
 host = DBConfig.service.get("host")
 port = DBConfig.service.get("port", default=3306)
 echo = DBConfig.service.get("echo", default=False, cast=bool_)
 database = DBConfig.service.get("database")
-
-tmp_mysql_uri = f"mysql+pymysql://{username}:{password}@{host}:{port}"
-tmp_engine = sa.create_engine(tmp_mysql_uri, echo=echo)
-
-
-with tmp_engine.connect() as conn:
-    conn.execute(sa.text(f"CREATE DATABASE IF NOT EXISTS {database}"))
 
 mysql_uri = ic(f"mysql+pymysql://{username}:{password}@{host}:{port}/{database}")
 engine = sa.create_engine(mysql_uri, echo=echo)
